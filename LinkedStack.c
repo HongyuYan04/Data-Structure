96877ef7a504ed5dc35bdf05081f1cd857e9bb64
HongyuYan04 <3059628698@qq.com>
2025-02-24 16:51:49 +0800

    Update LinkedStack.c

--- LinkedStack.c
+++ LinkedStack.c
@@ -7,24 +7,24 @@
 // LinkedStack`s structure definition is as follows :
 #define ElemType int
 typedef struct LinkNode {
-	ElemType data;
-	struct LinkNode* next;
+    ElemType data;
+    struct LinkNode* next;
 } LinkedStack;
 
 // Initial the LinkedStack L, which has a head node.
 bool InitStack(LinkedStack **stack) {
-	*stack = (LinkedStack *) malloc (sizeof(LinkedStack));
-	if (*stack == NULL) {
-		return false;
-	}
-	(*stack)->next = NULL;
-	return true;
+    *stack = (LinkedStack *) malloc (sizeof(LinkedStack));
+    if (*stack == NULL) {
+        return false;
+    }
+    (*stack)->next = NULL;
+    return true;
 }
 
 // If L has not a head node, the InitStack function is :
 bool InitStack(LinkedStack **stack) {
-	*stack = NULL;
-	return true;
+    *stack = NULL;
+    return true;
 }
 
 
