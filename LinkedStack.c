bc8de25d2e392f8c5330320a99c79e72bcf7d39b
HongyuYan04 <3059628698@qq.com>
2025-02-24 16:50:30 +0800

    Add files via upload

---
+++ LinkedStack.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+// include the headfile
+
+// LinkedStack`s structure definition is as follows :
+#define ElemType int
+typedef struct LinkNode {
+	ElemType data;
+	struct LinkNode* next;
+} LinkedStack;
+
+// Initial the LinkedStack L, which has a head node.
+bool InitStack(LinkedStack **stack) {
+	*stack = (LinkedStack *) malloc (sizeof(LinkedStack));
+	if (*stack == NULL) {
+		return false;
+	}
+	(*stack)->next = NULL;
+	return true;
+}
+
+// If L has not a head node, the InitStack function is :
+bool InitStack(LinkedStack **stack) {
+	*stack = NULL;
+	return true;
+}
+
+
