bf852ccac7e93b596785323f76c0663ff4bc2a19
HongyuYan04 <3059628698@qq.com>
2025-02-27 15:34:47 +0800

    Create 队列的顺序实现.cpp

---
+++ 代码模板/队列的顺序实现.cpp
@@ -0,0 +1,23 @@
+#define MaxSize 50
+typedef struct {
+    ElemType data[MaxSize];
+    int front, rear;
+} SqQueue;
+
+void InitQueue(SqQueue &Q) {
+    Q.rear = Q.front = 0;
+}
+
+bool QueueEmpty(SqQueue Q) {
+    return Q.rear == Q.front;
+}
+
+bool Enqueue(SqQueue &Q) {
+    // add an element
+    if ((rear + 1) % MaxSize == front) {
+        return false;
+    }
+    Q.data[Q.rear] = x;
+    Q.rear = (Q.rear + 1) % MaxSize;
+    return true;
+}
