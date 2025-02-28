d62f1f5d420455c3676deaf45e43db5bfc2a5714
HongyuYan04 <3059628698@qq.com>
2025-02-28 19:45:54 +0800

    Update 二叉树的遍历.cpp

--- 代码模板/二叉树的遍历.cpp
+++ 代码模板/二叉树的遍历.cpp
@@ -71,3 +71,62 @@ int maxDepth(struct TreeNode* root) {
         return L > R ? L + 1 : R + 1;       
     }
 }
+
+// 二叉树的层次遍历
+typedef struct LinkNode {
+    BiTNode *data;
+    struct LinkNode *next;
+} LinkNode;
+
+typedef struct {
+    LinkNode *front, *rear;
+} LinkQueue;
+
+typedef struct BiTNode {
+    char data;
+    struct BiTNode *lchild, *rchild;
+} BiTNode, *BiTree;
+
+void InitQueue(LinkQueue &Q) {
+    Q.front = Q.rear = (LinkNode *) malloc (sizeof(LinkNode));
+    Q.front->next = NULL;
+}
+
+bool IsEmpty(LinkQueue Q) {
+    return Q.front == Q.rear;
+}
+
+void EnQueue(LinkQueue &Q, BiTNode *x) {
+    LinkNode *p = (LinkNode *) malloc (sizeof(LinkNode));
+    p->data = x;
+    p->next = NULL;
+    Q.rear->next = p;
+    Q.rear = p;
+}
+
+void DeQueue(LinkQueue &Q, BiTNode* &ans) {
+    LinkNode *p = Q.front->next;
+    ans = p->data;
+    Q.front->next = p->next;
+    if (Q.rear == p) {
+        Q.rear = Q.front;
+    }
+    free(p);
+}
+
+void LevelOrder(BiTree T) {
+    LinkQueue Q;
+    InitQueue(Q);
+    BiTNode *p;
+    EnQueue(Q, T);
+    while (!IsEmpty(Q)) {
+        DeQueue(Q, p);
+        visit(p);
+        if (p->lchild != NULL) {
+            EnQueue(Q, p->lchild);
+        }
+        if (p->rchild != NULL) {
+            EnQueue(Q, p->rchild);
+        }
+    }
+}
