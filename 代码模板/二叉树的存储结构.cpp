24cb76a0377f0e60c412204a37467343d474fe8d
HongyuYan04 <3059628698@qq.com>
2025-02-28 19:08:52 +0800

    Update 二叉树的存储结构.cpp

--- 代码模板/二叉树的存储结构.cpp
+++ 代码模板/二叉树的存储结构.cpp
@@ -5,3 +5,26 @@ typedef struct {
     bool IsEmpty;
 } TreeNode;
 TreeNode T[MaxSize];
+
+// 链式存储 (二叉链表)
+struct ElemType {
+    int value;    
+};
+
+typedef struct BiTNode {
+    // 结构定义
+    ElemType data;
+    struct BiTNode *lchild, *rchild;
+} BiTNode, *BiTree;
+
+BiTree Root = NULL;
+Root = (BiTree) malloc (sizeof(BiTNode));
+Root->data = {1};
+Root->lchild = NULL;
+Root->rchild = NULL;
+
+BiTNode *p = (BiTNode *) malloc (sizeof(BiTNode));
+p->data = {2};
+p->lchild = NULL;
+p->rchild = NULL;
+Root->lchild = p;
