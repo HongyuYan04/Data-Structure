a727a6161e67c1ce3613acc35157af26ba6ae537
HongyuYan04 <3059628698@qq.com>
2025-03-02 22:06:56 +0800

    Update 二叉排序树.cpp

--- 代码模板/二叉排序树.cpp
+++ 代码模板/二叉排序树.cpp
@@ -7,3 +7,40 @@
       } BSTNode, *BSTree;
 
 */ 
+
+BSTNode* BST_Search(BSTree T, ElemType key) {
+    //    二叉排序树的查找 (非递归)
+    while (T != NULL && key != T->value) {
+        if (key < T->value) {
+            T = T->lchild;
+        } else {
+            T = T->rchild;
+        }
+    }
+    return T;
+}
+
+BSTNode* BST_Search(BSTree T, ElemType key) {
+    //    二叉排序树的查找 (递归)
+    if (T == NULL || key == T->value) {
+        return T;
+    }
+    if (key < T->key) {
+        return BST_Search(T->lchild, key);
+    } else {
+        return BST_Search(T->rchild, key);
+    }
+    return T;
+}
+
+bool BST_Insert(BSTree &T, ElemType x) {
+    //    二叉排序树的插入 (递归)
+    if (T == NULL) {
+        T = (BSTNode *) malloc (sizeof(BSTNode));
+        T->value = x;
+        T->lchild = NULL;
+        T->rchild = NULL;
+        return true;
+    }
+    if (k == T->
+}
